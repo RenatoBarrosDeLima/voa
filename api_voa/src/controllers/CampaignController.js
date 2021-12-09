62b09b6422a978f0784a11a396f7e74190faf885
unknown <renato.dev.mobi@gmail.com>
2021-12-09 14:23:43 -0300

    Add retorno de estorno na rota de pesquisa do uusário - backend

--- api_voa/src/controllers/CampaignController.js
+++ api_voa/src/controllers/CampaignController.js
@@ -126,7 +126,7 @@ class CampaignController {
         attributes: ['id', 'title'],
         include: {
           model: Donation,
-          attributes: ['id', 'value', 'created_at'],
+          attributes: ['id', 'value', 'created_at', 'reversal'],
           include: {
             model: User,
             attributes: ['id', 'name', 'email', 'cpf', 'telephone'],
@@ -162,7 +162,7 @@ class CampaignController {
             },
             reversal,
           },
-          attributes: ['id', 'value', 'created_at'],
+          attributes: ['id', 'value', 'created_at', 'reversal'],
           include: {
             model: User,
             attributes: ['id', 'name', 'email', 'cpf', 'telephone'],
